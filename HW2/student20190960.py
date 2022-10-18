b1f979d1ecdcae6431001b8da9edc12f559bfd26
Jiyoung Moon <daldongsan420@gmail.com>
2022-10-19 00:21:31 +0900

    update

--- HW2/student20190960.py
+++ HW2/student20190960.py
@@ -1,49 +1,49 @@
 #!usr/bin/python3
 import openpyxl
-from openpyxl.utils import get_column_letter
 
-wb = openpyxl.load( "student.xlsx" )
+wb = openpyxl.load_workbook("student.xlsx")
 ws = wb['Sheet1']
 
-row_num = 2
-While True:
-	col_midterm = sheet_ranges.cell(row = row_num, column = 3).value
-	col_final = Sheet_ranges.cell(row = row_num, colume = 4).value
-	col_homework = sheet_ranges.cell(row = row_num, column = 5).value
-	col_attendance = sheet_ranges.cell(row = row_num, colume = 6).value
+list_grade = []
+list_total = []
 
-	if col_midterm == None:
-		break
+row_id = 1
+for row in ws:
+	if row_id != 1:
+		sum_v = ws.cell(row = row_id, column = 3).value * 0.3
+		sum_v += ws.cell(row = row_id, column = 4).value * 0.35
+		sum_v += ws.cell(row = row_id, column = 5).value * 0.34
+		sum_v += ws.cell(row = row_id, column = 6).value
+		ws.cell(row = row_id, column = 7).value = sum_v
+		list_grade.append([row_id, sum_v])
+	row_id += 1
 	
-	col_total = col_midterm * 0.3 + col_final * 0.35 + col_homework * 0.34 + col_attendance
+list_total = sorted(list_grade, key = lambda x:x[1])
+a = int(len(list_total) * 0.3)
+ap = int(a * 0.5)
+b = int((len(list_total) * 0.7) - a)
+bp = int(b * 0.5)
+c = int(len(list_total) - a -b)
+cp = int(c * 0.5)
 
-	col_grade = 'Q'
 
-	sheet_ranges.cell(row = row_num, column = 7, value = col_total)
-	sheet_ranges.cell(row = row_num, column = 8, value = col_grade)
-	row_num += 1
 
-stu_num = row_num - 2
-AP = int(stu_num * 0.15)
-A = int(stu_num * 0.3) - AP
-BP = int(stu_num * 0.2)
-B = int(stu_num * 0.4) - BP
-re = stu_num - AP - A - BP - B - F
-CP = re // 2
-C = re - CP
-
-grade_num = [AP, A, BP, B, CP, C]
-print(grade_num)
-
-num = F
-n = 0
-while num != stu_num:
-	best = -100
-	index = -1
-	i = -1
-	for i in range (2, row_num):
-		if best < sheet_ranges.cell(row = i, column = 7).value:
-			if sheet_ranges.cell(row = i, column = 8).value != 'Q':
-				continue
-		num += 1
-wb.save(filename = "output.xlsx")
+for i in range(len(list_total)):
+	if i <= ap:
+		ws.cell(row = list_total[i][0], column = 8).value = 'A+'
+	elif i <= a:
+		ws.cell(row = list_total[i][0], column = 8).value = 'A0'
+	elif i <= bp:
+		ws.cell(row = list_total[i][0], column = 8).value = 'B+'
+	elif i <= b:
+		ws.cell(row = list_total[i][0], column = 8).value = 'B0'
+	elif i <= cp:
+		ws.cell(row = list_total[i][0], column = 8).value = 'C+'
+	elif i <= c:
+		ws.cell(row = list_total[i][0], column = 8).value = 'C0'
+	i += 1
+wb.save("student.xlsx")
+wb.close()
+		
+		
+			
