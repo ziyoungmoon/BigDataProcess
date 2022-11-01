2299ca109dac820c8ce8dff950b21442bc00d4b4
Jiyoung Moon <daldongsan420@gmail.com>
2022-11-02 00:04:54 +0900

    fix

--- HW3/UBERStudent20190960.py
+++ HW3/UBERStudent20190960.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python
 import sys
-from datetime import datetime, date
-
-def what_day_is_it(date):
-        days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
-        day = date.weekday()
-        return days[day]
-
-read = sys.argv[1]
-write = sys.argv[2]
+import datetime
+days = ['MON', 'TUE', 'WED', 'THR', 'FRI', 'SAT', 'SUN']
 uberDic = {}
+try:
+	inputFile = open(sys.argv[1], "rt")
+	outputFile = open(sys.argv[2], "wt")
+	for line in inputFile:
+		s = line.split(",")
+		a = line.split(",")[1].split("/")
+		day = days[datetime.date(int(a[2]), int(a[0]), int(a[1])).weekday()]
+		
+		keyStr = s[0] + "," + day
+		if keyStr not in uberDic:
+			uberDic[keyStr] = (s[2] + "," + s[3])
+		else:
+			v = int(uberDic[keyStr].split(",")[0]) + int(s[2])
+			t = int(uberDic[keyStr].split(",")[1]) + int(s[3])
+			uberDic[keyStr] = (str(v) + "," + str(t))				
+	for key, value in uberDic.items():
+		outputStr = key + " " + value + "\n"
+		outputFile.write(outputStr)
+except FileNotFoundError:
+	print("FileNotFoundError")
+finally:
+	inputFile.close
+	outputFile.close
 
-fp = open(read, "rt")
-for line in fp :
-    s_arr = line.split(',')
-    date_str = s_arr[1].split('/')
-    date = what_day_is_it(int(date_str[2], int(date_str[0]), int(date_str[1])))
-
-    key = s_arr[0] + "," + date
-	if key not in uberDic:
-	    uberDic[key] = (s_arr[2] + "," + s_arr[3])
-	else:
-		v = int(uberDic[key].split(",")[0]) + int(s_arr[2])
-		t = int(uberDic[key].split(",")[1]) + int(s_arr[3])
-		uberDic[key] = (s_arr(v) + "," + s_arr(t))				
-fp.close()
-
-f = open(write, 'wt')
-for key, count in uberDic.items():
-    f.write(key[0] + "," + key[1])
-    f.write(" %d,%d\n" % (count[0], count[1]))
-f.close()
